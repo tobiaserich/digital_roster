5c0d60aa812ab487fc0c7a32d3432cc440caff14
tobiaserich <tobiaserich@hotmail.de>
2021-08-08 18:49:38 +0200

    Fetch roster and save in a state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import getRoster from "./assets/getRoster";
+import { SinglePersonRoster } from "./assets/getRoster";
 
-function App() {
-  const fetchRoster = async () => {
-    const result = await getRoster();
-  };
+const App = (): any => {
+  const [roster, setRoster] =
+    React.useState<{ cluster: string; employees: SinglePersonRoster[] }[]>();
 
-  fetchRoster();
+  React.useEffect(() => {
+    const fetchRoster = async () => {
+      const result = await getRoster();
+      setRoster(result);
+    };
+    fetchRoster();
+  }, []);
+
+  console.log(roster);
   return <>Hello World</>;
-}
+};
 
 export default App;
